e098b26ff9ac25d4cdf1827349019eca4bdff479
DominicGalan <ndpgalan.student@ua.edu.ph>
2024-04-11 14:56:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import './App.css';
 
 function Galan({ display }) {
   return (
-    <div style={{ backgroundColor: 'orange', height: '500px', width: '500px', display: 'flex', fontWeight: 'bold', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', fontFamily: 'Arial, sans-serif'}}>
-      <p> {display} <br/> world</p>
+    <div className='Galan'>
+      {display}
+    </div>
+  );
+}
+
+function Galan2() {
+  return (
+    <div class="Galan2">
+      <div className='top'>
+        <div className='tl'>
+          <div class="item1">1</div>
+          <div class="item2">2</div>
+        </div>
+        <div className='tr'>
+          <div class="item3">3</div>  
+          <div class="item4">4</div>
+        </div>
+      </div>
+
+      <div className='bottom'>
+        <div class="item5">5</div>
+        <div class="item6">6</div>
+        <div class="item7">7</div>  
+        <div class="item8">8</div>
+      </div>
     </div>
   );
 }
@@ -11,7 +35,8 @@ function Galan({ display }) {
 export default function App() {
   return (
     <div>
-      <Galan display="hello"/>
+      <Galan display="hello world"/>
+      <Galan2/>
     </div>
   );
 }
